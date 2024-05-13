b477f3abff74cc880d04f9eb82cb8977eee00123
VersaNotVice <168625633+VersaNotVice@users.noreply.github.com>
2024-05-13 08:47:45 +0200

    Add files via upload

---
+++ BuilderExample.cs
@@ -0,0 +1,80 @@
+﻿public interface IMailConstructor
+{
+    public IMailConstructor AddRecipient(string recipient);
+    public IMailConstructor AddSubject(string subject);
+    public IMailConstructor AddContent(string content);
+    public IMailConstructor AddAttachments(string attachments);
+    public IMailConstructor Reset();
+    public Mail Construct();
+}
+public class Mail
+{
+    //builder
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public string Recipient { get; set; }
+    public string Attachments { get; set; }
+}
+
+public class MailConstructor : IMailConstructor
+{
+    Mail mail;
+    public MailConstructor()
+    {
+        mail = new Mail();
+    }
+    public IMailConstructor AddSubject(string subject)
+    {
+        mail.Subject = subject;
+        return this;
+    }
+    public IMailConstructor AddRecipient(string recipient)
+    {
+        mail.Recipient = recipient;
+        return this;
+    }
+    public IMailConstructor AddContent(string content) 
+    {
+        mail.Content = content;
+        return this;
+    }
+
+    public IMailConstructor AddAttachments(string attachment)
+    {
+        mail.Attachments = attachment;
+        return this;
+    }
+    public IMailConstructor Reset()
+    {
+        mail = new Mail();
+        return this;
+    }
+
+    public Mail Construct()
+    {
+        return mail;
+    }
+}
+
+public class SMTP
+{
+    IMailConstructor mailConstructor;
+    public SMTP(IMailConstructor mailConstructor)
+    {
+        this.mailConstructor = mailConstructor;
+    }
+
+    public void SendNoReplyMail()
+    {
+        mailConstructor.AddSubject("No Reply").AddContent("Hello World").Construct();
+        //Sending logic here
+    }
+}
+public static class ClientCode
+{
+    public static void Run()
+    {
+        SMTP smtp = new SMTP(new MailConstructor());
+        smtp.SendNoReplyMail();
+    }
+}
---
+++ MethodFactoryExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThePattern
+{
+    //MethodFactory
+    public class WebElement
+    {
+        string name;
+        public WebElement(string name)
+        {
+            Console.WriteLine($"Found {name}");
+            this.name = name;
+        }
+        public void Click()
+        {
+            Console.WriteLine($"Clicked {name}");
+        }
+    }
+
+    public interface LoginPage
+    {
+        public WebElement loginButton();
+        public WebElement usernameInput();
+        public WebElement passwordInput();
+    }
+
+    public class ChromeLoginPage : LoginPage
+    {
+        public WebElement loginButton() { return new WebElement("#LoginButton"); }
+        public WebElement usernameInput() { return new WebElement("#userInput"); }
+        public WebElement passwordInput() { return new WebElement("#passwordElement"); }
+    }
+
+    public abstract class LoginPageFactory
+    {
+        public abstract LoginPage CreatePage();
+    }
+
+    public class ChromeLoginPageFactory : LoginPageFactory
+    {
+        public override LoginPage CreatePage()
+        {
+            return new ChromeLoginPage();
+        }
+    }
+
+    public static class ClientCode
+    {
+        public static void Run()
+        {
+            LoginPageFactory loginPageFactory = new ChromeLoginPageFactory();            
+            LoginPage loginPage = loginPageFactory.CreatePage();
+            loginPage.loginButton().Click();
+        }
+    }
+}
